73f25fd07311c9a04e120709facb018ace9b0b5d
henao88 <jhenaohenao@gmail.com>
2021-04-30 02:38:15 -0500

    another commit - Badges

--- src/components/badge.js
+++ src/components/badge.js
@@ -4,21 +4,24 @@ import '../styles/Badge.css';
 
 
 class Badge extends React.Component {
-  render() {
+    render() {
+        // this.props
+        // const nombre = 'Julian';
+        // const apellido = 'Henao H.';
     return (
       <div className="badge">
         <div className="badge_header">
         <img src={confLogo} alt="Logo de la conferencia" />
         </div>
         <div className="badge_section-name">
-          <img className="badge_avatar" src="https://s.gravatar.com/avatar/89e0d1c13aec05d2fc71c206e04242a9?s=80" alt="Avatar" />
+          <img className="badge_avatar" src={this.props.avatarUrl} alt="Avatar" />
           <h1>
-            Julian <br /> Henao
+            {this.props.nombre} <br /> {this.props.apellido}
           </h1>
         </div>
         <div className="badge_section-info ">
-          <h3>frontend Engineer</h3>
-          <div>@Henao88</div>
+          <h3>{this.props.jobTitle}</h3>
+          <div>@{this.props.twitter}</div>
         </div>
         <div className="badge_footer" >#platziconf</div>
       </div>
--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css';
-import Badge from "./components/badge";
+import "bootstrap/dist/css/bootstrap.css";
 
+import "./global.css";
+import Badge from "./components/badge";
 
 // const jsx = <h1>Hello, Platzi Badges 2021!</h1>;
 // const element = React.createElement(
@@ -24,4 +25,13 @@ import Badge from "./components/badge";
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badge />, container);
+ReactDOM.render(
+  <Badge
+    nombre="Julian"
+    apellido="Henao"
+    avatarUrl="https://s.gravatar.com/avatar/89e0d1c13aec05d2fc71c206e04242a9?"
+    jobTitle="Frontend Engineer - Professor"
+    twitter="Henao88"
+  />,
+  container
+);
